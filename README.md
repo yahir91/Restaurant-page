96c87af9af512c8f8546a5dae7a0f593a6e7cc2b
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-11 17:33:59 -0600

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Restaurant-page
+# Restaurant Page
+
+### This is a simple restaurant webpage where you can toogle between the menu and contact. It was created to showcase the use of webpack and modular programming.
+
+![screenshot](screenshot.png)
+
+### Built with
+- JavaScript Vanilla
+- HTML5 
+- CSS3
+- Bootstrap
+- Webpack
+
+#### Livedemo
+- [Live Demo](https://elegant-bell-3db233.netlify.app/)
+
+
+## Getting started
+- run `https://github.com/yahir91/Restaurant-page`;
+- run `npm install`
+- run `npm run build`
+- open `dist/index.html`
+
+## Author
+- [Yahir Cardona](https://yahir91.github.io/yahir-cardona.github.io/)
+
+
+## Contributing
+
+Contributions, issues and feature requests are welcome!
+
+
+## Show your support
+
+Give a star if you like this project!
+
+
+## 📝 License
+
+This project is MIT licensed.
--- src/index.js
+++ src/index.js
@@ -9,36 +9,3 @@ import { menuListener } from './modules/menuButton';
 render();
 contactListener();
 menuListener();
-
-
-// const content = document.querySelector('#content')
-
-// const heading = document.createElement('h1')
-// heading.textContent = 'Restaurant Page'
-// heading.classList.add('display-1', 'd-flex', 'justify-content-center')
-// content.appendChild(heading)
-
-
-// const buttonContainer = document.createElement('div')
-// const contactButton = document.createElement('button')
-// const menuButton = document.createElement('button')
-// buttonContainer.classList.add('row', 'd-flex', 'justify-content-center')
-// contactButton.classList.add('btn', 'btn-primary', 'mx-2')
-// menuButton.classList.add('btn', 'btn-primary', 'mx-2')
-// contactButton.textContent = 'Contact'
-// menuButton.textContent = 'Menu'
-// contactButton.setAttribute('type', 'button')
-// menuButton.setAttribute('type', 'button')
-// menuButton.textContent = 'Menu'
-// content.appendChild(buttonContainer)
-// buttonContainer.appendChild(menuButton)
-// buttonContainer.appendChild(contactButton)
-// headerCreation(content)
-// buttonCreation(content)
-
-
-// menuCreation()
-
-// const render = () => {
-//     if (contactContainer == null)
-// }
